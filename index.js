021dc4e28bd40a4a9ea30fffc68b4b505d7fc100
kelvinobuvie <kelvinobuvie@gmil.com>
2023-09-07 19:51:49 +0100

    Frontend Task 1

---
+++ index.js
@@ -0,0 +1,9 @@
+const dayNameEl = document.querySelector(".day");
+const UTCTime = document.querySelector(".time");
+
+const date = new Date();
+
+dayNameEl.innerText = date.toLocaleDateString("en", {
+    weekday: "long"
+})
+UTCTime.innerHTML = date.getMilliseconds();
