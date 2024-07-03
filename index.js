c3cfa664c6b296c9de9807ed3b1099c7abad6544
kelvin obuvie <kelvinobuvie@gmail.com>
2024-07-03 12:17:34 +0100

    made changes to the file and added a new picture

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const UTCTime = document.querySelector(".time");
 const date = new Date();
 
 dayNameEl.innerText = date.toLocaleDateString("en", {
-    weekday: "long"
-})
-UTCTime.innerHTML = date.getTime();
+	weekday: "long",
+});
+function updateTime() {
+	const now = new Date();
+	const utcHours = String(now.getUTCHours()).padStart(2, "0");
+	const utcMinutes = String(now.getUTCMinutes()).padStart(2, "0");
+	const utcSeconds = String(now.getUTCSeconds()).padStart(2, "0");
+	const utcTime = `${utcHours}:${utcMinutes}:${utcSeconds}`;
+	document.getElementById("time").textContent = utcTime;
+}
+
+setInterval(updateTime, 1000);
+updateTime();
